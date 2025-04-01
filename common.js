2f686120c3629e32b499568c84597406fcbd9c58
Jianan Lai <dlai211@uw.edu>
2025-04-02 01:39:42 +0200

    commit all

---
+++ common.js
@@ -0,0 +1,88 @@
+let currentCut = null;
+let imageData = imageMap[currentCut] || { images: [], title: '' };
+
+// Get references to DOM elements
+const imageContainer = document.getElementById("image-container");
+const cutTitle = document.getElementById("cut-title");
+const ul = document.getElementById("cut-links");
+let imagesPerRow = 2; // Default number of images per row
+
+// Function to update images based on selected cut
+function updateImages(cut_name) {
+    imageContainer.innerHTML = "";
+    cutTitle.textContent = imageData.title;
+
+    document.querySelectorAll("#cut-nav a").forEach(a => a.classList.remove("active"));
+    document.querySelectorAll(`a[data-cut='${cut_name}']`).forEach(a => a.classList.add("active"));
+
+    imageContainer.style.display = "grid";
+    imageContainer.style.gridTemplateColumns = `repeat(${imagesPerRow}, 1fr)`;
+
+    const images = imageData.images.map(file => `${imageData.path}/${file}`);
+    images.forEach((img) => {
+        const container = document.createElement('div');
+        container.className = 'image-container';
+
+        const imgElement = document.createElement('img');
+        imgElement.src = img;
+        imgElement.alt = img.split('/').pop();
+        imgElement.onclick = () => openModal(img); // click to zoom
+
+        const filename = document.createElement('p');
+        filename.className = 'filename';
+        filename.textContent = img.split('/').pop();
+
+        container.appendChild(imgElement);
+        container.appendChild(filename);
+        imageContainer.appendChild(container);
+
+    })
+}
+
+
+// Change the images per row based on the mode selected 
+document.querySelectorAll('.mode-btn').forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        imagesPerRow = parseInt(btn.textContent);
+        document.querySelectorAll('.mode-btn').forEach(b => b.classList.remove('active'));
+        btn.classList.add('active');
+        updateImages(currentCut);
+    });
+});
+
+
+const li = document.createElement("li");
+Object.keys(cut_config).forEach((cut, index, array) => {
+    // Create clickable cut name
+    const a = document.createElement("a");
+    a.href = "#";
+    a.textContent = cut;
+    a.dataset.cut = cut;
+    a.onclick = () => {
+        currentCut = cut;
+        updateImages(currentCut);
+    }
+    a.style.margin = "0 5px"; // Add some spacing
+
+    li.appendChild(a);
+});
+ul.appendChild(li);
+
+// // Modal Function
+function openModal(src) {
+    const modal = document.getElementById("image-modal");
+    document.getElementById("modal-img").src = src;
+    modal.classList.add("show"); // Only add the class now
+}
+
+// Close Modal When Clicking Outside Image
+document.querySelector(".close").onclick = function () {
+    document.getElementById("image-modal").classList.remove("show");
+};
+
+document.getElementById("image-modal").onclick = function (event) {
+    if (event.target === this) {
+        this.classList.remove("show");
+    }
+};
---
+++ config_common.js
@@ -0,0 +1,51 @@
+
+const cut_config = {
+    "barrel_module": true, "annulus_module": true, "strip_hits": true, "traccc_athena": true
+};
+
+const barrel_image = [
+    "barrel_barrel_ec.png",              "barrel_phiStripPatternCentre.png",
+"barrel_cells.png",                  "barrel_phi_module.png",
+"barrel_etaPitch.png",               "barrel_shift.png",
+"barrel_etaStripPatternCentre.png",  "barrel_stripLength.png",
+"barrel_eta_module.png",             "barrel_stripPitch.png",
+"barrel_layer_disk.png",             "barrel_thickness.png",
+"barrel_length.png",                 "barrel_width.png",
+"barrel_phiPitch.png"
+];
+
+const annulus_image = [
+    "annulus_barrel_ec.png",   "annulus_length.png",      "annulus_thickness.png",
+"annulus_cells.png",       "annulus_phi_module.png",  "annulus_width.png",
+"annulus_eta_module.png",  "annulus_shift.png",
+"annulus_layer_disk.png",  "annulus_stripPitch.png"
+];
+
+const traccc_athena_image = [
+    "diff_mea_ec.png",  "diff_mea_ec_log.png",  "diff_num_mea.png",  "diff_num_mea_ec.png"
+];
+
+const strip_hits_image= [];
+
+const imageMap = {
+    "barrel_module": {
+        images: barrel_image,
+        path: 'plots/barrel',
+        title: 'Barrel Module'
+    },
+    "annulus_module": {
+        images: annulus_image,
+        path: 'plots/annulus',
+        title: 'Annulus Module'
+    },
+    "traccc_athena": {
+        images: traccc_athena_image,
+        path: 'plots/traccc_athena',
+        title: 'Traccc vs. Athena'
+    },
+    "strip_hits": {
+        images: strip_hits_image,
+        path: 'plots/strip_hits',
+        title: 'Strip Hits'
+    }
+};  
