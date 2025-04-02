278f2a0d8790e53751d10894ba53e47317c8f97a
Jianan Lai <dlai211@uw.edu>
2025-04-02 13:16:44 +0200

    commit all

--- config_common.js
+++ config_common.js
@@ -4,28 +4,47 @@ const cut_config = {
 };
 
 const barrel_image = [
-    "barrel_barrel_ec.png",              "barrel_phiStripPatternCentre.png",
-"barrel_cells.png",                  "barrel_phi_module.png",
-"barrel_etaPitch.png",               "barrel_shift.png",
-"barrel_etaStripPatternCentre.png",  "barrel_stripLength.png",
-"barrel_eta_module.png",             "barrel_stripPitch.png",
-"barrel_layer_disk.png",             "barrel_thickness.png",
-"barrel_length.png",                 "barrel_width.png",
-"barrel_phiPitch.png"
+'barrel_width.png',
+'barrel_length.png',
+'barrel_cells.png',
+'barrel_shift.png',
+'barrel_stripPitch.png',
+'barrel_stripLength.png',
+'barrel_phiStripPatternCentre.png',
+'barrel_etaStripPatternCentre.png',
+'barrel_phiPitch.png',
+'barrel_etaPitch.png',
+'barrel_thickness.png',
+'barrel_barrel_ec.png',
+'barrel_layer_disk.png',
+'barrel_phi_module.png',
+'barrel_eta_module.png',
+'barrel_width_stripPitch.png',
+'barrel_length_stripPitch.png',
 ];
 
 const annulus_image = [
-    "annulus_barrel_ec.png",   "annulus_length.png",      "annulus_thickness.png",
-"annulus_cells.png",       "annulus_phi_module.png",  "annulus_width.png",
-"annulus_eta_module.png",  "annulus_shift.png",
-"annulus_layer_disk.png",  "annulus_stripPitch.png"
+'annulus_width.png',
+'annulus_length.png',
+'annulus_cells.png',
+'annulus_shift.png',
+'annulus_stripPitch.png',
+'annulus_thickness.png',
+'annulus_barrel_ec.png',
+'annulus_layer_disk.png',
+'annulus_phi_module.png',
+'annulus_eta_module.png',
+'annulus_width_stripPitch.png',
+'annulus_length_stripPitch.png',
 ];
 
 const traccc_athena_image = [
     "diff_mea_ec.png",  "diff_mea_ec_log.png",  "diff_num_mea.png",  "diff_num_mea_ec.png"
 ];
 
-const strip_hits_image= [];
+const strip_hits_image= [
+    "strip_localx_localy.png",  "strip_r_globalz.png",  "strip_stripID.png",
+];
 
 const imageMap = {
     "barrel_module": {
